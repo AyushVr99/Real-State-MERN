ba03a9ae2ca6acf4d3a2750e7cd7b06e5e0237f1
Ayush Verma <ayushver9918@gmail.com>
2024-07-26 20:40:41 +0530

    real-state-project

---
+++ api/index.js
@@ -0,0 +1,42 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from "./routes/listing.route.js";
+import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
+dotenv.config(); 
+
+mongoose
+    .connect(process.env.MONGO).then(()=>{
+        console.log("MongoDb connected!");
+    })
+    .catch((err) => {
+        console.log(err);  
+    });
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT, () => {
+    console.log(`Server started at PORT: ${PORT}`);
+})
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
